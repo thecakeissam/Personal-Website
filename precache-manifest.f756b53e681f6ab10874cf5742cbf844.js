ccc660ebb3201329dc22978eb6072e26828e7f67
Sam MacGinty <smacgint@nd.edu>
2020-01-08 14:10:32 +0000

    Updates

---
+++ precache-manifest.f756b53e681f6ab10874cf5742cbf844.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3283c5ab2ea502b1c2cfe98990206ce8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2440d0756779e21b767d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "161ecb31d8003f016690",
+    "url": "/static/js/2.5ea0a261.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5ea0a261.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2440d0756779e21b767d",
+    "url": "/static/js/main.7620d10d.chunk.js"
+  },
+  {
+    "revision": "3877348275e8a43c71af",
+    "url": "/static/js/runtime-main.9270ea39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
